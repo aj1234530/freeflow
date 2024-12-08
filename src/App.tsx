1a0297c61fea05f56a2fc9834ff09e4b32129bae
mainabhihun <youmailmejust123@gmail.com>
2024-12-08 23:28:46 +0530

    the rect getting drawn only when the the button clicked

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import './App.css'
 
 function App() {
   const [buttonClicked, setButtonClicked] = useState(false)
-
+  // To avoid running effect on initial load
+  const handleButtonClick = () => {
+    setButtonClicked(true)
+  }
   useEffect(() => {
     console.log("ran")
-    const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-    if (canvas) {
-      const ctx = canvas.getContext('2d');
-      // Example: Draw a rectangle
-      if (!ctx) {
-        console.log('failed to load the context')
-        return
+    if (buttonClicked) {
+      const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+      if (canvas) {
+        const ctx = canvas.getContext('2d');
+        // Example: Draw a rectangle
+        if (!ctx) {
+          console.log('failed to load the context')
+          return
+        }
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(50, 50, 100, 100);
       }
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(50, 50, 100, 100);
     }
+
   }, [buttonClicked]);
 
 
@@ -28,7 +34,7 @@ function App() {
       {/* writing the js in the jsx */}
 
       {/* { const ctx = canvas.getContext('2d'); } js statement not allowed only express that evaluates to the value*/}
-      <button onClick={() => setButtonClicked(true)}>Draw Rectangle</button>
+      <button onClick={handleButtonClick}>Draw Rectangle</button>
     </>
   )
 }
